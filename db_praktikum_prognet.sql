eba1464858bcd420bcfe9d0109ab7838c899f783
darmaputra11 <61018282+darmaputra11@users.noreply.github.com>
2020-05-07 22:30:33 +0800

    Add files via upload

---
+++ db_praktikum_prognet.sql
@@ -0,0 +1,42 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.1.36-MariaDB : Database - db_praktikum_prognet
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_praktikum_prognet` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `db_praktikum_prognet`;
+
+/*Table structure for table `mahasiswas` */
+
+DROP TABLE IF EXISTS `mahasiswas`;
+
+CREATE TABLE `mahasiswas` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(30) DEFAULT NULL,
+  `nim` varchar(15) DEFAULT NULL,
+  `prodi` varchar(30) DEFAULT NULL,
+  `alamat_tinggal` varchar(70) DEFAULT NULL,
+  `no_hp` varchar(15) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `mahasiswas` */
+
+insert  into `mahasiswas`(`id`,`nama`,`nim`,`prodi`,`alamat_tinggal`,`no_hp`,`created_at`,`updated_at`) values (2,'Darme','1805551027','Teknologi Informasi','Klungkung','0812345678','2020-05-05 16:10:18','2020-05-05 16:10:18');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
